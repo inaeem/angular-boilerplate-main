fe1e636e469d2974b1926cd6cffcf41949163682
Code Monkey <inaeem@users.noreply.github.com>
2026-03-05 10:49:31 -0500

    v12

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, TranslateModule],
 })
 export class UsersModule {}
